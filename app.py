6914f7ac81a5bed32cd795cdf909f11fde2cb518
Charvi Attri <64918960+charvi-a@users.noreply.github.com>
2021-08-07 23:23:54 +0400

    Update

--- app.py
+++ app.py
@@ -1,34 +1,90 @@
 from flask import Flask,render_template
 import pandas as pd
+import numpy as np
 import folium
+import csv
+from geopy.exc import GeocoderTimedOut
+from geopy.geocoders import Nominatim
+import requests
 
-#Load the dataset.
-file = pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/12-31-2020.csv')
+url = "https://api.covid19api.com/summary"
+payload={}
+headers = {}
+response = requests.request("GET", url, headers=headers, data=payload)
+jsonResponse = response.json()
+
+csv_columns = ['Country','TotalConfirmed','CountryCode', 'NewDeaths', 'Date', 'TotalDeaths', 'NewConfirmed', 'Slug', 'NewRecovered', 'Premium', 'ID', 'TotalRecovered']
+country_data = jsonResponse["Countries"]
+csv_file = "data.csv"
+
+#convert JSON data into a csv file
+try:
+    with open(csv_file, 'w') as csvfile:
+    	writer = csv.DictWriter(csvfile, fieldnames=csv_columns)
+    	writer.writeheader()
+    	for i in range(len(country_data)):
+            writer.writerow(country_data[i])
+except IOError:
+    print("I/O error")
+
+file = pd.read_csv("data.csv", encoding = 'unicode_escape', engine ='python')
 
 def get_top_10(file):
 #getting the data based on the countries.
-	countries = file.groupby('Country_Region').sum()[['Confirmed']]
+	countries = file.groupby('Country').sum()[['TotalConfirmed']]
 #get the top 10 countries with the largest number of confirmed cases.
-	country_df = countries.nlargest(10,'Confirmed')[['Confirmed']]
+	country_df = countries.nlargest(10,'TotalConfirmed')[['TotalConfirmed']]
 	return country_df
 
 country_df = get_top_10(file)
 
 rows = []
-for country,Confirmed in zip(country_df.index,country_df['Confirmed']):
+for country,Confirmed in zip(country_df.index,country_df['TotalConfirmed']):
 	rows.append((country,Confirmed))
 
+map_countries=folium.Map(location=[34.223334,-82.461707],tiles='CartoDB positron',zoom_start=3)
 
+
+<<<<<<< HEAD
+latitudes = []
+longitudes = []
+def get_geolocation(country):
+	try:
+		geolocator = Nominatim(user_agent="COVID-19 Project")
+		return geolocator.geocode(country)
+	except GeocoderTimedOut:
+		return get_geolocation(country)
+=======
 map_countries=folium.Map(location=[34.223334,-82.461707], tiles='CartoDB positron',
                          zoom_start=3)
+>>>>>>> 992b92b083748a652246c20ee6388d2f556defec
 
+
+for ele in file["Country"]:
+	if get_geolocation(ele):
+		location = get_geolocation(ele)
+		longitudes.append(location.longitude)
+		latitudes.append(location.latitude)
+	#location not found
+	else:
+		longitudes.append(np.nan)
+		latitudes.append(np.nan)
+
+file["Longitude"] = longitudes
+file["Latitude"] = latitudes
 
 def create_circle(ele):
-	folium.Circle(location=[ele[0],ele[1]], radius=10000,fill = True,
-                    popup=f'{ele[3]} Confirmed Cases {ele[2]}', color='red').add_to(map_countries)
+	folium.Circle(location=[ele[0],ele[1]], radius=ele[2],fill = True,
+                    popup=f'{ele[4]} New Confirmed Cases {ele[2]} Total Confirmed Cases {ele[3]}', color='red').add_to(map_countries)
 
+file = file.dropna(subset=['Latitude','Longitude','NewConfirmed','TotalConfirmed','Country'])
+file[['Latitude','Longitude','NewConfirmed','TotalConfirmed','Country']].apply(lambda ele:create_circle(ele),axis=1)
+
+<<<<<<< HEAD
+=======
 file = file.dropna(subset=['Lat','Long_','Confirmed','Combined_Key'])
 file[['Lat','Long_','Confirmed','Combined_Key']].apply(lambda ele:create_circle(ele),axis=1)
+>>>>>>> 992b92b083748a652246c20ee6388d2f556defec
 html_map=map_countries._repr_html_()
 
 
@@ -39,5 +95,5 @@ def index():
 	return render_template("index.html",cmap=html_map,table=country_df,rows=rows)
 
 if __name__ == "__main__":
-	app.run(debug=True)
+	app.run(debug = False)
 
